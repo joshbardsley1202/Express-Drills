c9e3e6936bc349787c7dae0216bca4228e7e0fa5
joshbardsley1202 <jlbardsley@yahoo.com>
2018-08-08 15:59:22 -0600

    push repo to heroku

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require("express")
+const app = express()
+const cors = require("Cors")
+const port = process.env.PORT || 9000
+const cohortData = require("./cohorts.js")
+
+app.use(cors())
+
+function findById(data, id) {
+    for (let i = 0; i < data.length; i++) {
+        let idString = data[i].id.toString()
+        if (idString === id) {
+            return data[i];
+        }
+    }return null;
+}
+
+app.get("/", (req, res, next) => {
+    res.json({cohortData: cohortData});
+})
+
+app.get("/:id", (req, res,next) => {
+    const cohort = findById(cohortData, req.params.id);
+    if (!cohort) {
+        res.status(404).send({
+            error: {
+                message: "No record found!"
+            }
+        })
+    } else {
+        res.json({cohortData: cohort});
+    }
+});
+
+app.listen(port, () => {
+    console.log(`I am listening on ${port}`)
+})
