282f691f787f6614be334f601bff3e885dbdae7c
joshbardsley1202 <jlbardsley@yahoo.com>
2018-08-08 16:28:56 -0600

    Drill updated with heroku

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const app = express()
-const cors = require("Cors")
+const cors = require("cors")
 const port = process.env.PORT || 9000
 const cohortData = require("./cohorts.js")
 
